e4374cfef47da484b692a803e8e309ce10f6a4b7
Chris Laughter <chrislaugh@gmail.com>
2019-09-01 14:26:40 -0600

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,15 @@
     var userName = prompt( 'Enter you name?' );
     var likeGames = confirm ("Do you like Hangman? Hit Cancel if no." );
     
-    // Global variables
-    var words // make list of songs
-    var letters = [];
+    // Global variables for game
+    var words = [ 'In A Gadda Da Vida, Tales of Brave Ulysses, If 6 Was 9, Yellow Submarine, One of These Days, Waiting for the Sun, The Low Spark of High Heeled Boys, The Great Gig in the Sky ' ];// song list
+    var letters = [ 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'];
+    var actualWord
+
+    // Go in Scorboard
     var wins = 0;
     var losses = 0;
-    var actualWord
+    
 
     // Respond to user input
     if(likeGames) {
@@ -25,11 +28,12 @@
         alert('Let\'s Go!');
       } else {
         window.open( 'https://claughter.github.io/basic-portfolio/' );
-      }
-      
+      }      
       
     // Place userName after Hello in gameArea
     document.getElementById( 'welcome' ).innerHTML="Hello " + userName + "!";
+
+// ------------------------------------------------------------
 
   // Start game
     function startGame() {
@@ -39,60 +43,3 @@
       }
     }
     
-    
-    // use actualWord as reference
-    // append
-
-
-// Build alphabet array
-// 
-
-// Create a reset button
-// Create replay button
-
-// var gameMechanics = {
-  // DOM ELEMENTS
-//   BLANK_SPACE: $("#blankSpace"),
-//   DEBUG: $("#debug"),
-
-//   // Game Properties
-//   words: ["ryan", "mark", "matthew", "james"],
-//   current_word: "",
-//   blankSpace: "",
-
-//   chooseWord: function() {
-//     var blankSpace = "";
-//     var chooseWord = this.words[Math.floor(Math.random() * this.words.length)];
-    
-//     /** 
-//      * NOTE: If you are refering to a variable inside THIS object you need to use 'this' keyword.
-//      */
-//     this.current_word = chooseWord;
-//     this.debugThis(this.current_word);
-
-//     // INSERT BLANK SPACE
-//     for (var i = 0; i < chooseWord.length; i++) {
-//       var x = chooseWord.charAt(i);
-//       if (x === "") {
-//         this.blankSpace += x;
-//       } else {
-//         this.blankSpace += " _ "
-//       }
-//     }
-		
-//     this.BLANK_SPACE.html(this.blankSpace);
-
-//   },
-
-//   debugThis: function(debug_string) {
-//     this.DEBUG.html(debug_string);
-//     console.log("======================");
-//     console.log(debug_string);
-//     console.log("======================");
-//   },
-
-// };
-
-// $(document).ready(function() {
-//   gameMechanics.chooseWord();
-// });
