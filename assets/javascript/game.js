9975dcc59e40b4d5448df95cd03f131c10e886c3
Chris Laughter <chrislaugh@gmail.com>
2019-09-01 16:15:41 -0600

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,10 @@
     var likeGames = confirm ("Do you like Hangman? Hit Cancel if no.");
     
     // Global variables for game 
-    // gameArea      
+    // game area      
     var letters = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'];
     var actualWord
-    // ScoreArea
+    // Scoreboard area
     var wins = 0;
     var losses = 0;
     
@@ -29,7 +29,18 @@
       //   window.open('https://claughter.github.io/basic-portfolio/');
       // }      
       
-    // Place userName after Hello in gameArea
+    // Place userName after Hello in game area heading
     document.getElementById('welcome').innerHTML = "Hello " + userName + "! " + "Press any key to get started.";
 
 // ------------------------------------------------------------
+
+  // Start game
+  function startGame() {
+  // Word list     
+  var words = ['In A Gadda Da Vida, Tales of Brave Ulysses, If 6 Was 9, Yellow Submarine, One of These Days, Waiting for the Sun, The Low Spark of High Heeled Boys, The Great Gig in the Sky'];
+
+// Computer randon word choice  
+  actualWord=words[Math.floor(Math.random() * words.length)]
+  for(var i=0; i < actualWord.length; i++) {
+  progressWord.push('-');
+  }
