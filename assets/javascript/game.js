23f006ebcb16c7eab09e65e375eb2c58f27a5cff
Chris Laughter <chrislaugh@gmail.com>
2019-09-05 10:48:30 -0600

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,14 +22,14 @@ document.getElementById('welcome').innerHTML = 'Hello ' + userName + '! ' + 'Pre
 
 // instructions in gameArea
 // document.getElementById('underScore').innerHTML = 'Press any lowerCase key to get started.';
-
 // Display right wrong boxes
-document.getElementById('right').innerHTML = 'Right Guess';
+document.getElementById('guessedLtrs').innerHTML = 'Guessed Letters';
 document.getElementById('wrong').innerHTML = 'Wrong Guess'
+document.getElementById('right').innerHTML = 'Right Guess';
 
 // Display ScoreBoard
 document.getElementsByClassName('score').innerHTML = 'Scoreboard';
-document.getElementById('remaining').innerHTML = 'Remaining guesses: ';
+// document.getElementById('remaining').innerHTML = 'Remaining guesses: ';
 document.getElementById('win').innerHTML = 'Wins: ';
 document.getElementById('loss').innerHTML = 'Losses: ';
 
@@ -40,7 +40,7 @@ document.getElementById('loss').innerHTML = 'Losses: ';
 // Create an array of words
 const words = ['stones', 'cream', 'hendrix', 'beatles', 'floyd', 'doors', 'traffic', 'heep'];  
 let letters = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'];
-let actualWord = "";
+let actualWord = '';
 let currentWrdLtrs = [];
 let underScores = [];
 // let rightChoice = [];
@@ -77,10 +77,10 @@ function newGame() {
   }
     console.log(createUnderScore());
 
-    document.getElementById("underScore").innerHTML = underScores.join(" ");
-    document.getElementById("remaining").innerHTML = "Number of Guesses Remaining: " + " " + guessesLeft;
-    document.getElementById("win").innerHTML = "Wins: " + " " + wins;
-    document.getElementById("loss").innerHTML = "Losses: " + " " + losses;
+    document.getElementById('underScore').innerHTML = underScores.join(' ');
+    document.getElementById('remaining').innerHTML = 'Number of Guesses Remaining: ' + ' ' + guessesLeft;
+    document.getElementById('win').innerHTML = 'Wins: ' + ' ' + wins;
+    document.getElementById('loss').innerHTML = 'Losses: ' + ' ' + losses;
 
 }
 
@@ -117,42 +117,43 @@ function verifyLtrs() {
       
   } else { //If user input is not a letter from the alphabet
     // Alert the user
-    alert("Press a letter)");
+    alert('Press a letter)');
   }
 }
 
 function roundComplete() {
-  console.log("Win count: " + wins +  " | Loss Count: " + losses + " | Guesses Left: " + guessesLeft)
+  console.log('Win count: ' + wins +  ' | Loss Count: ' + losses + ' | Guesses Left: ' + guessesLeft)
 
   //Update HTML with Game Stats
-  document.getElementById("remaining").innerHTML = "Number of Guesses Remaining: " + " " + guessesLeft;
-  document.getElementById("underScore").innerHTML = underScores.join(" ");
-  document.getElementById("right").innerHTML = "Letters Already Guessed:" + " " + wrongChoice.join(" ");
+  document.getElementById('remaining').innerHTML = 'Number of Guesses Remaining: ' + ' ' + guessesLeft;
+  document.getElementById('underScore').innerHTML = underScores.join(' ');
+  document.getElementById('right').innerHTML = 'Letters Already Guessed:' + ' ' + wrongChoice.join(' ');
 
   //Check if the user won
-  if (currWrdLtrs.toString() == underScores.toString()) {
-    wins++;
+  if (computerSelect.toString() == underScores.toString()) {
+    win++;
     alert("CONTRATULATIONS! You guessed '" + word + "' correctly. Try another round?");
-    console.log("YOU WIN!");
+    console.log('YOU WIN!');
 
     // Update the wins in the HTML doc
-    document.getElementById("win").innerHTML = "Wins: " + " " + win;
+    document.getElementById('win').innerHTML = 'Wins: ' + ' ' + win;
 
     //Start New Game and clear letters already guessed
     newGame();
-    document.getElementById("guessedLetters").innerHTML = "Letters Already Guessed:" + " " + " ";
+    document.getElementById('guessedLtrs').innerHTML = 'Letters Already Guessed:' + ' ' + ' ';
 
   } else if (guessesLeft == 0) { //Check if user lost
-    losses++;
-    alert("OH NO! You have 0 guesses left, and all your friends are now in the upsidedown. The correct word was '" + word + "'. Do you want to try again?")
-    console.log("You Lost!");
+    loss++;
+    alert('OH NO! You have 0 guesses left, and all your friends are now in the upsidedown. The correct word was "' + currentWord + '". Do you want to try again?')
+			console.log('You Lost!');
+    console.log('You Lost!');
 
     // Update the wins in the HTML doc
-    document.getElementById("loss").innerHTML = "Losses: " + " " + loss;
+    document.getElementById('loss').innerHTML = 'Losses: ' + ' ' + loss;
 
     //Start New Game
     newGame();
-    document.getElementById("guessedLetters").innerHTML = "Letters Already Guessed:" + " " + " ";
+    document.getElementById('guessedLtrs').innerHTML = 'Letters Already Guessed:' + ' ' + ' ';
 
   }  
 }
@@ -167,52 +168,11 @@ function roundComplete() {
 	document.onkeyup = function(event) {
 		//Create a variable to hold all the letters that have been guessed
 		let ltrsGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-			console.log("You Guessed the letter: " + ltrsGuessed); // Testing via Console.Log
+			console.log('You Guessed the letter: ' + ltrsGuessed); // Testing via Console.Log
 
 		//Run the check letters function
 		checkLtrs(ltrsGuessed);
 		roundComplete();
-
-		//Game interation with the images (Guesses Left, as displayed by images)
-		if (guessesLeft <= 8) {
-			document.getElementById("elevenImg").style.transform = "rotate(180deg)";
-			document.getElementById("elevenImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 7) {
-			document.getElementById("mikeImg").style.transform = "rotate(180deg)";
-			document.getElementById("mikeImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 6) {
-			document.getElementById("willImg").style.transform = "rotate(180deg)";
-			document.getElementById("willImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 5) {
-			document.getElementById("dustinImg").style.transform = "rotate(180deg)";
-			document.getElementById("dustinImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 4) {
-			document.getElementById("lucasImg").style.transform = "rotate(180deg)";
-			document.getElementById("lucasImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 3) {
-			document.getElementById("madMaxImg").style.transform = "rotate(180deg)";
-			document.getElementById("madMaxImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 2) {
-			document.getElementById("steveImg").style.transform = "rotate(180deg)";
-			document.getElementById("steveImg").style.opacity = "0.25";
-		}
-
-		if (guessesLeft <= 1) {
-			document.getElementById("chiefHopperImg").style.transform = "rotate(180deg)";
-			document.getElementById("chiefHopperImg").style.opacity = "0.25";
-		}
   };
 
 
